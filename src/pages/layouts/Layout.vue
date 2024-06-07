d9efda10531db0c533ef9ad4ea634069d8122fcc
liuqi <13761299894@163.com>
2024-06-07 18:01:46 +0800

    vue blog 开发

---
+++ src/pages/layouts/Layout.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="layout-wrapper">
+        <div class="layout-header">header</div>
+        <div class="layout-content">
+            <router-view></router-view>
+        </div>
+        <div class="layout-footer"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Layout",
+    data() {
+        return {}
+    }
+}
+</script>
+
+<style>
+</style>
