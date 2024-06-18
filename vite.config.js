b40039fed209f7b3ebc49b7d0dbdd3b1b3d4daa7
liuqi <13761299894@163.com>
2024-06-18 14:59:07 +0800

    增加less处理

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,18 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue(), vueJsx()],
-	resolve: {
-		alias: {
-			'@': fileURLToPath(new URL('./src', import.meta.url))
-		}
-	}
+    plugins: [vue(), vueJsx()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    css: {
+        preprocessorOptions: {
+            less: {
+                additionalData: `@import "src/common/common.less";`
+                // 或其他配置选项
+            }
+        }
+    }
 });
