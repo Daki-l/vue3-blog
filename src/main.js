94e90eae88349039cca758c1b923cb0ebd8f751f
liuqi <13761299894@163.com>
2024-06-18 17:45:32 +0800

    增加mockJs

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { createPinia } from 'pinia';
 import ElementPlus from 'element-plus';
 import router from './router';
 import App from './App.vue';
+import './mock';
 
 import 'moment/locale/zh-cn';
 moment.locale('zh-cn');
