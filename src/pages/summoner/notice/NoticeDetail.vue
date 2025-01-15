3c22a41269f8373efaeb0f14b6fce493d9cb4a3d
liuqi <13761299894@163.com>
2025-01-15 18:03:35 +0800

    活动详情开发

---
+++ src/pages/summoner/notice/NoticeDetail.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="notice_detail">
+        <div class="notice_title">{{ 'xxx' }}</div>
+    </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+import { summpnerStore } from '@/stores/summoner.js'
+import { useRoute, useRouter } from 'vue-router';
+import { getNoticeFn, getNoticesActivityFn } from '@/services/summonerServices/summonerServices.js';
+import { ElMessage } from 'element-plus';
+
+const noticeDetail = ref({});
+onMounted(() => {
+    console.log('onMounted--',summpnerStore.noticeDetail );
+})
+</script>
+
+<style></style>
