1ee7df2c8c7d64320e90f131692b69512bafbbd9
liuqi <13761299894@163.com>
2025-01-24 16:30:16 +0800

    新增文件

---
+++ src/pages/summoner/About.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="about_wrapper">
+        <div class="about_text">关于页面</div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="less" scoped>
+.about_wrapper {
+
+}
+</style>
